3cd72a81a6e8d1f50b582d4450f205f97031dc47
Anton Galitsyn <anton.galitsyn@gmail.com>
2025-01-01 14:55:13 +0700

    solve day 2: part 1

--- advent-of-code/2024/02/solution.py
+++ advent-of-code/2024/02/solution.py
@@ -9,13 +9,45 @@ DEBUG = os.getenv("DEBUG", False)
 
 
 def part_1(data: list[str]) -> int:
+    safe_reports = 0
     for line in data:
-        items = line.split()
-        increasing = all(i < j for i, j in zip(items, items[1:]))
-        decreasing = all(i < j for i, j in zip(items, items[1:]))
-        res = increasing or decreasing
-        print(res)
-    return 0
+        items = [int(v) for v in line.split()]
+        increasing = items[0] < items[1]
+        decreasing = items[0] > items[1]
+        if DEBUG:
+            print(items, "increasing:", increasing, "decreasing:", decreasing)
+
+        if increasing or decreasing:
+            verdict = False
+            prev = items[0]
+            for i in items[1:]:
+                verdict = False
+
+                # filter out lines like `86441`
+                if i == prev:
+                    break
+
+                diff = abs(i - prev)
+                if DEBUG:
+                    print(i, "prev =", prev, "diff =", diff)
+
+                # check increasing and decreasing constantly and check adjacent levels
+                if increasing and i > prev and diff >= 1 and diff <= 3:
+                    verdict = True
+                elif decreasing and i < prev and diff >= 1 and diff <= 3:
+                    verdict = True
+                else:
+                    # means there is no sense to continue, this lline is not valid
+                    break
+
+                prev = i
+
+            if DEBUG:
+                print(verdict, "\n")
+            if verdict:
+                safe_reports += 1
+
+    return safe_reports
 
 
 def part_2(data: list[str]) -> int:
@@ -26,8 +58,11 @@ if __name__ == "__main__":
     file_path = os.path.join(CURRENT_DIR, "input.txt")
     if DEBUG:
         file_path = os.path.join(CURRENT_DIR, "example.txt")
+
     data = read_lines_from_file(file_path)
+
     res_1 = part_1(data)
     print("part 1:", res_1)
+
     res_2 = part_2(data)
     print("part 2:", res_2)
