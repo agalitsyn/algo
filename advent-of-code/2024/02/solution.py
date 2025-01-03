cf1adbac78d0139a7156e6b6d05756db1b23999e
Anton Galitsyn <anton.galitsyn@gmail.com>
2025-01-03 18:11:22 +0700

    solve day 2: part 2

--- advent-of-code/2024/02/solution.py
+++ advent-of-code/2024/02/solution.py
@@ -8,53 +8,21 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 DEBUG = os.getenv("DEBUG", False)
 
 
-def part_1(data: list[str]) -> int:
-    safe_reports = 0
-    for line in data:
-        items = [int(v) for v in line.split()]
-        increasing = items[0] < items[1]
-        decreasing = items[0] > items[1]
-        if DEBUG:
-            print(items, "increasing:", increasing, "decreasing:", decreasing)
-
-        if increasing or decreasing:
-            verdict = False
-            prev = items[0]
-            for i in items[1:]:
-                verdict = False
-
-                # filter out lines like `86441`
-                if i == prev:
-                    break
-
-                diff = abs(i - prev)
-                if DEBUG:
-                    print(i, "prev =", prev, "diff =", diff)
-
-                # check increasing and decreasing constantly and check adjacent levels
-                if increasing and i > prev and diff >= 1 and diff <= 3:
-                    verdict = True
-                elif decreasing and i < prev and diff >= 1 and diff <= 3:
-                    verdict = True
-                else:
-                    # means there is no sense to continue, this lline is not valid
-                    break
-
-                prev = i
-
-            if DEBUG:
-                print(verdict, "\n")
-            if verdict:
-                safe_reports += 1
-
-    return safe_reports
-
-
-def check(items: list[int]) -> bool:
+def is_safe(items: list[int]) -> bool:
     increasing = items[0] < items[1]
     decreasing = items[0] > items[1]
     if not (increasing or decreasing):
         return False
+
+    if DEBUG:
+        print(
+            "items",
+            [x for x in items],
+            "increasing",
+            increasing,
+            "decreasing",
+            decreasing,
+        )
 
     verdict = False
     prev = items[0]
@@ -75,28 +43,39 @@ def check(items: list[int]) -> bool:
         elif decreasing and i < prev and diff >= 1 and diff <= 3:
             verdict = True
         else:
-            # means there is no sense to continue, this lline is not valid
+            # means there is no sense to continue, this line is not valid
             return False
 
         prev = i
+
+    if DEBUG:
+        print(verdict, "\n")
     return verdict
 
 
-def part_2(data: list[str]) -> int:
-    safe_reports = 0
+def part_1(data: list[str]) -> int:
+    res = 0
     for line in data:
         items = [int(v) for v in line.split()]
-        if DEBUG:
-            print(items)
+        if is_safe(items):
+            res += 1
+    return res
 
-            verdict = check(items)
 
-            if DEBUG:
-                print(verdict, "\n")
-            if verdict:
-                safe_reports += 1
+def part_2(data: list[str]) -> int:
+    res = 0
+    for line in data:
+        items = [int(v) for v in line.split()]
+        if is_safe(items):
+            res += 1
+        else:
+            for i in range(len(items)):
+                extra = items[:i] + items[i + 1 :]
+                if is_safe(extra):
+                    res += 1
+                    break
 
-    return safe_reports
+    return res
 
 
 if __name__ == "__main__":
@@ -107,7 +86,7 @@ if __name__ == "__main__":
     data = read_lines_from_file(file_path)
 
     res_1 = part_1(data)
-    print("part 1:", res_1)
+    print("part 1:", res_1, "\n")
 
     res_2 = part_2(data)
     print("part 2:", res_2)
